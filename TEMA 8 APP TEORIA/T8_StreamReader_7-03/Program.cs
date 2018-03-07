f59b22a6950089520df3d57cf4e233a33a20c99a
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-03-07 11:58:25 +0100

    Añado ejemplo de la clase StreamReader para leer texto
    Tambien añado comentado el ejercicio de examen que tendré que realizar

---
+++ TEMA 8 APP TEORIA/T8_StreamReader_7-03/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace T8_StringReader_7_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = @"..\..\..\ficheros\entrada.txt";
+
+
+
+
+            using (StreamReader fichero = new StreamReader(ruta, Encoding.Default))
+            {
+                VerUnoAUno(fichero);
+                Console.ReadLine();
+                //Al leer uno el puntero se queda al final y entonces no podrá leer los demás.
+                //Esto se solucionaría cerrando el fichero y abriéndolo en cada método. Porque al abrirlo para leer el puntero vuelve a la primera posición
+                //O creando un objeto nuevo para cada método.
+
+                //O así --> Cambiando el puntero a 0 con la clase base Stream:
+                Stream s = fichero.BaseStream; //Propiedad que devuelve el flujo base del mismo objeto. (Flujo en el que está basado)
+                s.Seek(0, SeekOrigin.Begin); //Al moverlo de la clase base de ese flujo también lo mueves en tu objeto.
+
+                VerUnoAUnoEOF(fichero);
+                Console.ReadLine();
+                s.Seek(0, SeekOrigin.Begin);
+
+                VerLineaALinea(fichero);
+                Console.ReadLine();
+                s.Seek(0, SeekOrigin.Begin);
+
+                VerTodo(fichero);
+                s.Seek(0, SeekOrigin.Begin);
+
+            }
+
+            Console.ReadLine();
+
+            //EJERCICIO DE EXAMEN:
+            //Usuario, clave. Si el usuario y la clave está en el fichero de texto separado por "; ", le dejas entrar (iniciar sesión).
+            //Abre el fichero de texto, separa los campos de ";" y buscas si está.
+
+            //Buscar el usuario y la contraseña está a continuación
+        }
+
+        static void VerUnoAUno(StreamReader fichero)
+        {
+            int tmp;
+            while ((tmp = fichero.Read()) != -1)
+                Console.Write((char)tmp); //Al convertirlo a char ya se ven las letras, si no saldrían los enteros del ASCII que equivalen a ese char
+        }
+
+        static void VerUnoAUnoEOF(StreamReader fichero)
+        {
+            while (!fichero.EndOfStream)
+                Console.Write((char)fichero.Read()); 
+        }
+
+        static void VerLineaALinea(StreamReader fichero)
+        {
+            string linea = string.Empty;
+            while ((linea = fichero.ReadLine()) != null)
+                Console.Write(linea);
+        }
+
+        static void VerTodo(StreamReader fichero)
+        {
+            string todo = string.Empty;
+            todo = fichero.ReadToEnd();
+            Console.Write(todo);
+        }
+
+    }
+}
