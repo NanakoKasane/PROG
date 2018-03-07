018928b00818b83a44771cbc15fd6cdf86cdf881
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-03-07 09:56:06 +0100

    Añado explicación de los valores de la enumeración FileMode

--- TEMA 8 APP TEORIA/T8_LecturaEscritura_Stream_5-3/Program.cs
+++ TEMA 8 APP TEORIA/T8_LecturaEscritura_Stream_5-3/Program.cs
@@ -17,6 +17,13 @@ namespace T8_LecturaEscritura_Stream_5_3
             FileStream fichero = new FileStream(ruta, FileMode.Open); //Al abrirse el puntero está al principio y por defecto lo abre para leer.
             Listar(fichero);
             fichero.Close();
+
+            //ENUMERACIÓN FILEMODE:
+            //FileMode.Append; // Si no existe lo crea, y si existe, añade el texto al final.
+            //FileMode.Truncate; // Lo deja vacío el archivo. Debe existir
+            //FileMode.Create; //Lo crea si no existe y escribe en la posición 0. Si existe, escribe en la posición 0.
+            //FileMode.CreateNew; //Como create pero si ya existe, lanza una excepción.
+            //FileMode.Open; // Lo abre en modo lectura por defecto.
             
 
             Console.ReadLine();
@@ -27,6 +34,7 @@ namespace T8_LecturaEscritura_Stream_5_3
         {
             int caracter;
 
+            //Mejor con EndOfSream o try catch y en el catch decir que acabe/se cierre. -1 solo es para TEXTO.
             while ((caracter = flujo.ReadByte()) != -1) //Esto solo vale si la clase maneja texto porque no hay ASCII negativo, pero si lee Byte, puede estar -1 y no leería más
                 Console.Write((char)caracter); //Es decir, solo si es un fichero de texto.
             
