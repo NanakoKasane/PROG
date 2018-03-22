4396d00df0338468adbe38a0b4e054bb3ea24df6
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-03-22 13:32:35 +0100

    Añado el ejercicio 1 de WPF

---
+++ EJERCICIOS T9 WPF PROG/WPF_Ej1_SumaNnumeros/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Ej1_SumaNnumeros
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtN.Focus();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int nNumero = int.Parse(txtN.Text);
+                int resultado = 0;
+                for (int i = 1; i <= nNumero; i++)
+                {
+                    resultado += i;
+                }
+                lblResultado.Content = resultado;
+            }
+            catch
+            {
+                //Controlar que sea un número lo que introduzcas (Si entra aquí es que el int.Parse ha fallado).
+                MessageBox.Show("El formato no es correcto");
+                txtN.Focus();
+                txtN.SelectAll();
+            }
+  
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close(); //Cierra la ventana
+        }
+    }
+}
---
+++ EJERCICIOS T9 WPF PROG/WPF_Ej2_CalculaExpresionMatematica/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Ej2_CalculaExpresionMatematica
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtExpresion.Focus();
+        }
+    }
+
+    public class Calcular 
+    {
+        //No tendrá en cuenta las prioridades de los operadores. Así que se harán de 2 en 2 las operaciones
+        //Texto solo admite --> , para decimales. Números y las operaciones +, -, *, /
+
+
+    }
+
+
+}
