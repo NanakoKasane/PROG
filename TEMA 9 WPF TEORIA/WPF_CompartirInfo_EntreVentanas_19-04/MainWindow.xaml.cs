97e67d88addb41b9f5e31e267dfe2d7b3b7373a3
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-04-19 10:53:53 +0200

    Añado ejemplo de compartir información entre varias ventanas mediante un evento en la segunda ventana al que nos suscribimos en la ventana1

---
+++ TEMA 9 WPF TEORIA/WPF_CompartirInfo_EntreVentanas_19-04/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CompartirInfo_EntreVentanas_19_04
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Ventana2 ventana2 = new Ventana2();
+
+            ventana2.OnEnviar += ventana2_OnEnviar;
+            ventana2.ShowDialog();
+        }
+
+        void ventana2_OnEnviar(string[] datos)
+        {
+            tblDato1.Text = datos[0];
+            tblDato2.Text = datos[1];
+        }
+    }
+}
---
+++ TEMA 9 WPF TEORIA/WPF_CompartirInfo_EntreVentanas_19-04/Ventana2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_CompartirInfo_EntreVentanas_19_04
+{
+    // Creo un delegado
+    public delegate void dlgEnviar(string[] datos);
+
+
+    /// <summary>
+    /// Lógica de interacción para Ventana2.xaml
+    /// </summary>
+    public partial class Ventana2 : Window
+    {
+        // Creo un evento basado en el delegado
+        public event dlgEnviar OnEnviar;
+
+        public Ventana2()
+        {
+            InitializeComponent();
+            tbxDato1.Focus();
+        }
+
+        private void btnEnviarDatos_Click(object sender, RoutedEventArgs e)
+        {
+            string[] datos = { tbxDato1.Text, tbxDato2.Text };
+
+            if (OnEnviar != null)
+            {
+                OnEnviar(datos);
+            }
+        }
+    }
+}
