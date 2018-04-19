0103fc2f7c87a660c99e576267537d7cafe4b187
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-04-19 10:53:06 +0200

    Añado ejemplo de Menú con comandos

---
+++ TEMA 9 WPF TEORIA/WPF_Menu_Comandos_19-04/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Menu_Comandos_19_04
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
