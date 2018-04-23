3d11165868df4b7df60c4689b1df68325bd2820d
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-04-23 11:12:15 +0200

    Añado ejemplo de Sincronizar datos con Binding
    Con una clase Film en otro espacio de nombres y sincronizar el nombre de la Film con un TestBlock

---
+++ TEMA 9 WPF TEORIA/WPF_Binding_SincronizarDatos_23-04/Film.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marina.WPF_Binding_SincronizarDatos_23_04
+{        
+    
+    public enum Genero { Mafia, Drama, Catastrofismo, Aventuras, Comedia, Oeste, Ciencia_Ficcion };
+
+    class Film
+    {
+        string _titulo;
+        Genero _genero;
+        bool? _oscar; // Lo mismo que poner Nullable<bool>
+        double _calificacion;
+
+        #region Propiedaes
+        public string Titulo
+        {
+            get { return _titulo; }
+            set { _titulo = value; }
+        }
+
+        public Genero Genero
+        {
+            get { return _genero; }
+            set { _genero = value; }
+        }
+
+        public bool? Oscar
+        {
+            get { return _oscar; }
+            set { _oscar = value; }
+        }
+
+        public double Calificacion
+        {
+            get { return _calificacion; }
+            set { _calificacion = value; }
+        }
+        #endregion 
+
+        // Tiene que tener un constructor por defecto vacío para poder usarlo en XML
+        public Film() { }
+        public Film(string titulo, Genero genero, bool? oscar, double calificacion)
+        {
+            this._titulo = titulo;
+            this._genero = genero;
+            this._oscar = oscar;
+            this._calificacion = calificacion;
+        }
+
+    }
+}
