05c21cb7e0aa004c952d1fdef0f273f9badd4dc2
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-04-25 09:54:43 +0200

    Añado ejercicio 12 de RGB con 3 scrollBar

---
+++ EJERCICIOS T9 WPF PROG/WPF_Ej12_RGB_ConScrolls/WPF_Ej12_RGB_ConScrolls/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Ej12_RGB_ConScrolls
+{
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Evento que comparten los 3 ScrollBars y se sincroniza con el valor por el que vaya cada scrollBar
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void scb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			Color color = Color.FromArgb(255, (byte)scb_Rojo.Value, (byte)scb_Verde.Value, (byte)scb_Azul.Value);
+
+			pnl_Color.Background = new SolidColorBrush(color);
+
+		}
+	}
+}
