a22b022179b04304e656241e1113904e0af0f5f5
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-05-02 12:34:28 +0200

    Añado ejemplo de clase ListaPersonas que cambia el contexto cada vez que se pase a la siguiente Persona

---
+++ TEMA 9 WPF TEORIA/WPF_DataContext_Personas_02-05/ListaPersonas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//--------------------------
+using System.Collections.ObjectModel;
+
+namespace Marina.WPF_DataContext_Personas_02_05
+{
+    class ListaPersonas : ObservableCollection<Persona>
+    {
+        Random rnd = new Random();
+        
+        // Crear lista de personas:
+        public ListaPersonas()
+            : base()
+        {
+            this.Add(new Persona("1. Pepe", "Castano", CrearFechaNacimiento(), CrearEstatura()));
+            this.Add(new Persona("2. Antonio", "Nocasta", CrearFechaNacimiento(), CrearEstatura()));
+            this.Add(new Persona("3. Alberto", "Cadiscos", CrearFechaNacimiento(), CrearEstatura()));
+            this.Add(new Persona("4. Serafin", "Demes", CrearFechaNacimiento(), CrearEstatura()));
+            this.Add(new Persona("5. Javi", "Sama Amado", CrearFechaNacimiento(), CrearEstatura()));
+
+        }
+
+        private DateTime CrearFechaNacimiento()
+        {
+            // Fecha de Nacimiento de los últimos 20 años:
+            DateTime fecha = DateTime.Now - TimeSpan.FromDays(rnd.Next(1, 365 * 20));
+            return fecha;
+        }
+
+        private double CrearEstatura()
+        {
+            // Estatura desde 1.50 a 2.10:
+            double estatura = rnd.Next(150, 211);
+            return estatura / 100;
+        }
+    }
+}
---
+++ TEMA 9 WPF TEORIA/WPF_DataContext_Personas_02-05/Persona.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//----------------------------
+using System.ComponentModel;
+
+namespace Marina.WPF_DataContext_Personas_02_05
+{
+    class Persona : INotifyPropertyChanged
+    {
+        string _nombre;
+        string _apellidos;
+        DateTime _fechaNacimiento;
+        double _estatura;
+
+        public Persona()
+        {
+            _nombre = "Eliseo";
+            _apellidos = "Moreno";
+            _fechaNacimiento = DateTime.Parse("01/01/1970");
+            _estatura = 0.0;
+        }
+
+        public Persona(string nombre, string apellidos, DateTime fechaNac, double estatura)
+        {
+            this._nombre = nombre;
+            this._apellidos = apellidos;
+            this._fechaNacimiento = fechaNac;
+            this._estatura = estatura;
+        }
+
+        #region Propiedades
+        public string Nombre
+        {
+            get { return _nombre; }
+            set {   _nombre = value;
+                    MetodoAuxiliarCambio("Nombre");
+                }
+        }
+        public string Apellidos
+        {
+            get { return _apellidos; }
+            set
+            {
+                _apellidos = value;
+                MetodoAuxiliarCambio("Apellidos");
+            }
+        }
+        public DateTime FechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            set { _fechaNacimiento = value; }
+        }
+        public double Estatura
+        {
+            get { return _estatura; }
+            set { _estatura = value; }
+        }
+        #endregion 
+
+    
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void MetodoAuxiliarCambio(string propagarPropiedad) // onPropertyChanged
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propagarPropiedad));
+        }
+
+    }
+}
