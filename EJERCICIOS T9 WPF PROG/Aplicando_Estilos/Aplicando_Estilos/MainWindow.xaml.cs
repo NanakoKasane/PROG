3ea7b80698689ea99d9fc17d2dff9e6145ea9937
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-05-02 12:36:51 +0200

    Añado ejemplo de examen de aplicar estilos

---
+++ EJERCICIOS T9 WPF PROG/Aplicando_Estilos/Aplicando_Estilos/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aplicando_Estilos
+{
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			lblFuentes.ItemsSource = Fonts.SystemFontFamilies;
+		}
+
+		private void cbx_negrita_Checked_1(object sender, RoutedEventArgs e)
+		{
+			if (cbx_cursiva.IsChecked == true)
+				tbx_AplicarEstilos.Style = (Style)Resources["NegritaCursiva"];
+			else
+				tbx_AplicarEstilos.Style = (Style)Resources["Negrita"];
+		}
+
+		private void cbx_negrita_Unchecked_1(object sender, RoutedEventArgs e)
+		{
+			if (cbx_cursiva.IsChecked == false)
+				tbx_AplicarEstilos.Style = null;
+			else
+				tbx_AplicarEstilos.Style = Resources["Cursiva"] as Style;
+		}
+
+		private void lblFuentes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			tbx_AplicarEstilos.FontFamily = lblFuentes.SelectedItem as FontFamily;
+		}
+
+		private void cbx_cursiva_Checked_1(object sender, RoutedEventArgs e)
+		{
+			if (cbx_negrita.IsChecked == true)
+				tbx_AplicarEstilos.Style = Resources["NegritaCursiva"] as Style;
+			else
+				tbx_AplicarEstilos.Style = Resources["Cursiva"] as Style;
+		}
+
+		private void cbx_cursiva_Unchecked_1(object sender, RoutedEventArgs e)
+		{
+			if (cbx_negrita.IsChecked == false)
+				tbx_AplicarEstilos.Style = null;
+			else
+				tbx_AplicarEstilos.Style = Resources["Negrita"] as Style;
+		}
+
+		private void cbx_sobra_Checked_1(object sender, RoutedEventArgs e)
+		{
+			grd_textBlock.Style = (Style)Resources["Sombreado"];
+		}
+
+		private void cbx_sobra_Unchecked_1(object sender, RoutedEventArgs e)
+		{
+			grd_textBlock.Style = null;
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			Propina ventanaPropina = new Propina();
+			ventanaPropina.Show();
+		}
+	}
+}
---
+++ EJERCICIOS T9 WPF PROG/Aplicando_Estilos/Aplicando_Estilos/Propina.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+//----------------------------
+using System.Text.RegularExpressions;
+
+namespace Aplicando_Estilos
+{
+	/// <summary>
+	/// Lógica de interacción para Propina.xaml
+	/// </summary>
+	public partial class Propina : Window
+	{
+		public Propina()
+		{
+			InitializeComponent();
+			tbx_importeFactura.Focus();
+		}
+
+		private void rbn_bueno_Checked_1(object sender, RoutedEventArgs e)
+		{
+			CalcularImportes(10);
+		}
+
+		private void rbn_muyBueno_Checked_1(object sender, RoutedEventArgs e)
+		{
+			CalcularImportes(15);
+		}
+
+		private void rbn_excelente_Checked_1(object sender, RoutedEventArgs e)
+		{
+			CalcularImportes(20);
+		}
+
+		private void CalcularImportes(double porcentaje)
+		{
+			double propina = 0;
+			double totalPagar = 0;
+
+			try
+			{
+				propina = Math.Round(double.Parse(tbx_importeFactura.Text) * (porcentaje/100), 2);
+				totalPagar = propina + double.Parse(tbx_importeFactura.Text);
+			}
+			catch
+			{
+				MessageBox.Show("Importe de la factura inválido");
+				tbx_importeFactura.Focus();
+				tbx_importeFactura.SelectAll();
+			}
+
+			EstablecerImportes(propina, totalPagar);
+		}
+
+		private void EstablecerImportes(double propina, double totalPagar)
+		{
+
+			tbl_propina.Text = propina.ToString();
+			tbl_totalPagar.Text = totalPagar.ToString();
+		}
+
+		private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			MessageBoxResult resultado = MessageBox.Show("¿Seguro que quiere salir?", "SALIR", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+			if (resultado == MessageBoxResult.No) // Si no quiere salir, cancelas la salida
+			{
+				e.Cancel = true;
+			}
+		}
+
+		private void tbx_importeFactura_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			Regex regla = new Regex("[0-9,]");
+
+			if (!regla.IsMatch(e.Text))
+				e.Handled = true;
+		}
+
+	}
+}
