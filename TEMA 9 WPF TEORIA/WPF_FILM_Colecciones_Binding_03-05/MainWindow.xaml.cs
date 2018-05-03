4216fc31c44b06c9fdc65a949311645767275de5
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-05-03 10:54:41 +0200

    Añado ejemplo de enlace de datos a una COLECCIÓN con la clase Director, que tiene una colección de objetos Film

---
+++ TEMA 9 WPF TEORIA/WPF_FILM_Colecciones_Binding_03-05/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//---------------------------
+using Marina.WPF_Binding_SincronizarDatos_23_04;
+
+namespace WPF_Binding_SincronizarDatos_23_04
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCambiarTitulo_Click(object sender, RoutedEventArgs e)
+        {
+            // Cambiamos algunas propiedades del objeto film creado como recurso accediento al mismo
+            // Cambia el valor de Titulo de la clase film 
+
+            Film film = Resources["film_Infiltrados"] as Film; // Sería como-> Film film = (Film)Resources["film_Infiltrados"];
+
+            if (film != null)
+            {
+                film.Titulo = "Titanic";
+                film.Genero = Genero.Catastrofismo;
+                film.Oscar = true;
+                film.Calificacion = 4.5;
+            }
+
+        }
+
+        private void btn_IrColecciones_Click(object sender, RoutedEventArgs e)
+        {
+            Win_Colecciones ventana_Colecciones = new Win_Colecciones();
+            ventana_Colecciones.ShowDialog();
+        }
+    }
+}
---
+++ TEMA 9 WPF TEORIA/WPF_FILM_Colecciones_Binding_03-05/Notificador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ---------------------------
+using System.ComponentModel;
+
+
+namespace Marina.WPF_Binding_SincronizarDatos_23_04
+{
+ 
+    /// <summary>
+    /// Clase usada para informar de los cambios del valor de las propiedades 
+    /// </summary>
+    public class Notificador : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged; // Del interfaz INotifyPropertyChanged, para que si cambies algo de la clase se propaguen los cambios a los elementos enlazados
+
+        // Método del evento:
+        protected void onPropertyChanged(string nombre)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(nombre));
+
+            
+        }
+    }
+    
+}
