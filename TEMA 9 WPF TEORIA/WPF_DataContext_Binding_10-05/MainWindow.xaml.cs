90eecf41d132e6da3069554ce97ab6a4507cc164
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-05-10 11:11:37 +0200

    Añado ejemplo de una primera plantilla sencila con Binding

---
+++ TEMA 9 WPF TEORIA/WPF_DataContext_Binding_10-05/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Personas_Binding_10_05
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cambiar_Click(object sender, RoutedEventArgs e)
+        {
+            // grdPrincipal.DataContext = "Broadway";
+
+            lbl_Texto.Content = "Broadway"; // Tiene prioridad al contexto de datos. Ahora el contexto de datos es Content
+            // Se aplica por prioridad -> Tiene más prioridad el más cercano al elemento. Si no lo encuentra ahí ya va buscando en sus padres (en este caso, buscaría en el DataContext)
+       
+        }
+    }
+}
