43312a83df985cb80a3bcab0587b8d2e0d9432b5
Marina Espinosa <marinaespinosagalvez@gmail.com>
2018-05-14 08:57:48 +0200

    Añado ejercicio de Matrices 2D

---
+++ EJERCICIOS T9 WPF PROG/WPF_Ej19_Matriz2DBotones/WPF_Ej19_Matriz2DBotones/CBotones.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//----------------------------
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace WPF_Ej19_Matriz2DBotones
+{
+	class CBotones
+	{
+		Grid rejilla = new Grid();
+		int nFilas;
+		int nColumnas;
+		const int _HEIGHTBOTON = 30;
+		const int _WIDTHBOTON = 30;
+		const int _MARGINBOTON = 2;
+
+
+		#region Constructores
+		public CBotones() { }
+
+		public CBotones(int nFilas, int nColumnas)
+		{
+			this.NFilas = nFilas;
+			this.NColumnas = nColumnas;
+		}
+		#endregion 
+
+		#region Propiedades
+		public int NFilas
+		{
+			get { return nFilas; }
+			set {
+					if (value > 20)
+						nFilas = 20;
+					else
+						nFilas = value; 
+				}
+		}
+
+		public int NColumnas
+		{
+			get { return nColumnas; }
+			set {
+					if (value > 20)
+						nColumnas = 20;
+					else
+						nColumnas = value; 
+				}
+		}
+
+		public Grid Rejilla
+		{
+			get { return rejilla; }
+			set { rejilla = value; }
+		}
+
+		public int HEIGHTBOTON
+		{
+			get { return _HEIGHTBOTON; }
+		}
+
+		public int WIDTHBOTON
+		{
+			get { return _WIDTHBOTON; }
+		}
+
+		public int MARGINBOTON
+		{
+			get { return _MARGINBOTON; }
+		} 
+		#endregion 
+		
+
+
+		public void RellenarRejilla()
+		{
+			// Le añado el número de columnas al Grid
+			for (int i = 0; i < nColumnas; i++)
+			{
+				Rejilla.ColumnDefinitions.Add(new ColumnDefinition());
+			}
+			
+			// Le añado el número de filas al Grid
+			for (int i = 0; i < nFilas; i++)
+			{
+				Rejilla.RowDefinitions.Add(new RowDefinition());
+			}
+			
+			
+			// Relleno el Grid con los botones
+			for (int i = 0; i < nFilas; i++)
+			{
+				for (int j = 0; j < nColumnas; j++)
+				{
+					Button btnTmp = new Button();
+					btnTmp.Name = string.Format("btn{0}_{1}", i, j); //btn0_1 será el botón de la fila 0 columna 1
+					btnTmp.Margin = new System.Windows.Thickness(MARGINBOTON); 
+					btnTmp.Content = string.Format("[{0},{1}]", i, j);
+					btnTmp.Height = HEIGHTBOTON;
+					btnTmp.Width = WIDTHBOTON;
+
+					// Eventos
+					btnTmp.Click += btnTmp_Click;
+					btnTmp.MouseLeave += btnTmp_MouseLeave;
+					btnTmp.MouseEnter += btnTmp_MouseEnter;
+					
+					// Lo añado a la rejilla
+					Grid.SetRow(btnTmp, i);
+					Grid.SetColumn(btnTmp, j);					
+					rejilla.Children.Add(btnTmp); 
+
+				}
+			}
+		}
+
+
+		// EVENTOS
+
+		void btnTmp_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			Button boton = sender as Button;
+			boton.Background = new SolidColorBrush(Colors.Red);
+		}
+
+		void btnTmp_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			Button boton = sender as Button;
+			boton.Background = new SolidColorBrush(Color.FromRgb(221, 221, 221));
+
+		}
+
+		void btnTmp_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			Button boton = sender as Button;
+			MessageBox.Show(boton.Name);		
+		}
+
+	}
+}
---
+++ EJERCICIOS T9 WPF PROG/WPF_Ej19_Matriz2DBotones/WPF_Ej19_Matriz2DBotones/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Ej19_Matriz2DBotones
+{
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			RellenarComboBox(cbx_nFilas);
+			RellenarComboBox(cbx_nColumnas);
+		}
+
+		private void RellenarComboBox(ComboBox cbx)
+		{
+			for (int i = 1; i <= 20; i++)
+			{
+				cbx.Items.Add(i);
+			}
+		}
+
+		private void btnCrearMatriz_Click(object sender, RoutedEventArgs e)
+		{
+			grd_botones.Children.Clear(); // Si hubiera alguna matriz, la limpio, para poder crear otra nueva
+			this.Height = 120; // Alto inicial
+			this.Width = 525; // Ancho inicial
+			CBotones matrizBotones = null;
+
+			try
+			{
+				matrizBotones = new CBotones(int.Parse(cbx_nFilas.Text), int.Parse(cbx_nColumnas.Text));			
+			}
+			catch { }
+
+			// Relleno la rejilla y expando la ventana según las filas y columnas que haya
+			if (matrizBotones != null)
+			{
+				matrizBotones.RellenarRejilla();
+
+				grd_botones.Children.Add(matrizBotones.Rejilla);
+				this.Height += matrizBotones.NFilas * (matrizBotones.HEIGHTBOTON + matrizBotones.MARGINBOTON);
+				this.Width += matrizBotones.NColumnas * (matrizBotones.WIDTHBOTON + matrizBotones.MARGINBOTON);
+
+			}
+
+
+		}
+
+	}
+}
